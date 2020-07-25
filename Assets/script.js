6e3ee8ba7beb147cbf70838aaf6f3b09e02067e9
Marc <mlanglois333@icloud.com>
2020-07-25 08:37:45 -0400

    Properly linked and displayed info from both API calls

--- Assets/script.js
+++ Assets/script.js
@@ -1,18 +1,44 @@
 
 var now= moment().format('MMMM Do, h:mm');
 var city="";
-
+let lon="";
+let lat="";
 
 
 function searchCity(){
 
     var citySearch= $("#searchVal").val();
     city= citySearch.trim();
-    renderWeather()
+
+    
+    getLat();
 }
 
+
+function getLat(){
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=516035025459d10bbc10ed362c69ce7b";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+        console.log(response);
+$("#cityTitle").html(response.name + " " + now);
+$("#windSpeed").text("Wind Speed: " + response.wind.speed);
+
+        lat=response.coord.lat;
+
+        lon=response.coord.lon;
+        renderWeather();
+
+});}
+
+
+
 function renderWeather(){
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=516035025459d10bbc10ed362c69ce7b";
+var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=516035025459d10bbc10ed362c69ce7b";
 
 $.ajax({
   url: queryURL,
@@ -22,13 +48,17 @@ $.ajax({
 
     console.log(response);
 
-    $("#cityTitle").html(response.name + " " + now);
-    $("#windSpeed").text("Wind Speed: " + response.wind.speed);
-    $("#humidity").text("Humidity: " + response.main.humidity);
-    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+    
+    
+    $("#humidity").text("Humidity: " + response.current.humidity);
+    var tempF = (response.current.temp - 273.15) * 1.80 + 32;
     $("#temperature").text("Temperature " + tempF.toFixed(2));
-    $("#UV").text("UV Index: " + response.main.UVindex);
-  });
+    $("#UV").text("UV Index: " + response.current.uvi);
+});
+
 }
+ 
+
+
 
 document.getElementById("searchBut").addEventListener("click", function(event) {event.preventDefault()});
