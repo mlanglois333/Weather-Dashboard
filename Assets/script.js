44b615b27b33c70223be8f16e4f62bf9d9501767
Marc <mlanglois333@icloud.com>
2020-07-27 18:24:36 -0400

    fixed icon ratio

--- Assets/script.js
+++ Assets/script.js
@@ -1,16 +1,16 @@
 
-var now= moment().format('MMMM Do, h:mm');
-var city="";
-let lon="";
-let lat="";
+var now = moment().format('MMMM Do, h:mm');
+var city = "";
+let lon = "";
+let lat = "";
 
-function clickBut(){
-console.log('clicked');
+function clickBut() {
+  console.log('clicked');
 }
 
-function cityBut(){
+function cityBut() {
 
-  var newBut = $('<input/>').attr({ type: 'button', text : city, value:city });;
+  var newBut = $('<input/>').attr({ type: 'button', text: city, value: city });;
   newBut.attr('class', 'cityBut');
 
   $('#searchCont').append(newBut);
@@ -19,80 +19,86 @@ function cityBut(){
 
 }
 
-function searchCity(){
+function searchCity() {
 
-    var citySearch= $("#searchVal").val();
-    city= citySearch.trim();
+  var citySearch = $("#searchVal").val();
+  city = citySearch.trim();
 
-    
-    getLat();
-    cityBut();
+
+  getLat();
+  cityBut();
 }
 
 
-function getLat(){
+function getLat() {
 
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=516035025459d10bbc10ed362c69ce7b";
+  var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=516035025459d10bbc10ed362c69ce7b";
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    })
-      .then(function(response) {
-        console.log(response);
-        var weathImg= "'http://openweathermap.org/img/wn/"+response.weather[0].icon +".png'"
-$("#cityTitle").html(response.name + " " + now + "<img src=" + weathImg + ">");
-
-
-        lat=response.coord.lat;
-
-        lon=response.coord.lon;
-        renderWeather();
-
-});}
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+      console.log(response);
+      var weathImg = "'http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png'"
+      $("#cityTitle").html(response.name + " " + now + "<img src=" + weathImg + ">");
 
 
+      lat = response.coord.lat;
 
-function renderWeather(){
-var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=516035025459d10bbc10ed362c69ce7b";
+      lon = response.coord.lon;
+      renderWeather();
 
-$.ajax({
-  url: queryURL,
-  method: "GET"
-})
-  .then(function(response) {
-
-    console.log(response);
-   
-        if (response.current.uvi <3.1){    $("#UV").html("UV Index: <span class='green'>" + response.current.uvi + "</span>");
-        ;}
-    else if (response.current.uvi <6.1){    $("#UV").html("UV Index: <span class='yellow'>" + response.current.uvi + "</span>");
-  }
-    else {    $("#UV").html("UV Index: <span class='red'>" + response.current.uvi + "</span>");
-    ;};
-    $("#windSpeed").text("Wind Speed: " + response.current.wind_speed);
-    $("#humidity").text("Humidity: " + response.current.humidity);
-    var tempF = (response.current.temp - 273.15) * 1.80 + 32;
-    $("#temperature").text("Temperature " + tempF.toFixed(2));
+    });
+}
 
 
 
+function renderWeather() {
+  var queryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude={part}&appid=516035025459d10bbc10ed362c69ce7b";
 
-    for (i=0;i<=5; i++){
-      var weathImg= "'http://openweathermap.org/img/wn/"+response.daily[i].weather[0].icon +".png'"
-      var tempF = (response.daily[i].temp.day - 273.15) * 1.80 + 32;
-     var day = moment().add(i, 'day').format('dddd');
-      $("#day" + i).html(day + "<br> <img src="+weathImg+"> <br> Temp: " + tempF.toFixed(2)+ " <br> Humidity: " + response.daily[i].humidity);
-      
-      
-    }
-});
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  })
+    .then(function (response) {
+
+      console.log(response);
+
+      if (response.current.uvi < 3.1) {
+        $("#UV").html("UV Index: <span class='green'>" + response.current.uvi + "</span>");
+        ;
+      }
+      else if (response.current.uvi < 6.1) {
+        $("#UV").html("UV Index: <span class='yellow'>" + response.current.uvi + "</span>");
+      }
+      else {
+        $("#UV").html("UV Index: <span class='red'>" + response.current.uvi + "</span>");
+        ;
+      };
+      $("#windSpeed").text("Wind Speed: " + response.current.wind_speed);
+      $("#humidity").text("Humidity: " + response.current.humidity);
+      var tempF = (response.current.temp - 273.15) * 1.80 + 32;
+      $("#temperature").text("Temperature " + tempF.toFixed(2));
+
+
+
+
+      for (i = 0; i <= 5; i++) {
+        var weathImg = "'http://openweathermap.org/img/wn/" + response.daily[i].weather[0].icon + "@2x.png'"
+        var tempF = (response.daily[i].temp.day - 273.15) * 1.80 + 32;
+        var day = moment().add(i, 'day').format('dddd');
+        $("#day" + i).html(day + "<br> <img src=" + weathImg + "> <br> Temp: " + tempF.toFixed(2) + " <br> Humidity: " + response.daily[i].humidity);
+
+
+      }
+    });
 
 }
- 
 
 
 
-document.getElementById("searchBut").addEventListener("click", function(event) {event.preventDefault()});
-document.getElementById("searchBut").addEventListener("click", function() {searchCity()});
+
+document.getElementById("searchBut").addEventListener("click", function (event) { event.preventDefault() });
+document.getElementById("searchBut").addEventListener("click", function () { searchCity() });
 
